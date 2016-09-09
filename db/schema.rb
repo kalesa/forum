002b821ec17e2d015d225b639f4f985bceef2d17
Marissa Abao <iza.abao@gmail.com>
2016-09-09 13:50:25 +0800

    added model post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908034548) do
+ActiveRecord::Schema.define(version: 20160909054748) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
