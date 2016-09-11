1fce8766b60661560ff757fd2b157af3928f3bc5
Marissa Abao <iza.abao@gmail.com>
2016-09-11 13:44:38 +0800

    5.3 added mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'iza.abao@gmail.com'
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
   get 'static_pages/index'
   get 'static_pages/about'
   get 'static_pages/contact'
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users
   root 'static_pages#index'
+  post 'static_pages/thank_you'
+  #resources :posts
+  #root 'posts#index'
   #root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
